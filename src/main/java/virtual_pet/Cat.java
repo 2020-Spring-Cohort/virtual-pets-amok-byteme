dea728cde895ed240c0f62712132d3673009013d
bwaterman8 <bwaterman8@gmail.com>
2020-01-21 11:15:47 -0500

    added attributes to Cat class and tests

--- src/main/java/virtual_pet/Cat.java
+++ src/main/java/virtual_pet/Cat.java
@@ -3,21 +3,52 @@ package virtual_pet;
 public class Cat {
 
     private int hunger;
+    private String name;
+    private int boredom;
+    private int health;
+    private int litterBox;
 
-    public Cat(String ben) {
-
+    public Cat(String name) {
+        this.name = name;
+        this. hunger = 10;
+        this.boredom = 10;
+        this.health = 10;
+        this.litterBox = 10;
+    }
+    public String getName() {
+        return name;
     }
 
     public int hunger () {
-        int hunger = 10;
         return hunger;
     }
 
-    public String getName() {
-        return "Ben";
+    public int boredom (){
+        return boredom;
     }
 
     public void feed() {
         hunger -= 2;
     }
+
+    public void play(){
+        boredom -= 2;
+    }
+
+    public int health(){
+        return health;
+    }
+
+    public void vetVisit(){
+        health += 5;
+    }
+
+    public int litterBox(){
+        return litterBox;
+    }
+
+    public void cleanLitterBox(){
+        litterBox -= 10;
+    }
+
 }
--- src/test/java/virtual_pet/CatTest.java
+++ src/test/java/virtual_pet/CatTest.java
@@ -30,4 +30,43 @@ public class CatTest {
     assertEquals( 8, result);
 
     }
+    @Test
+    public void petShouldHaveDefaultHealth(){
+    Cat underTest = new Cat("Ben");
+    int result = underTest.health();
+    assertEquals(10, result);
+    }
+    @Test
+    public void vetVisitShouldIncreaseHealthBy5(){
+    Cat underTest = new Cat ("Ben");
+    underTest.vetVisit();
+    int result = underTest.health();
+    assertEquals(15, result);
+    }
+    @Test
+    public void petShouldHaveDefaultBoredom(){
+    Cat underTest = new Cat ("Ben");
+    int result = underTest.boredom();
+    assertEquals(10, result);
+    }
+    @Test
+    public void playShouldDecreaseBoredomBy2(){
+    Cat underTest = new Cat ("Ben");
+    underTest.play();
+    int result = underTest.boredom();
+    assertEquals(8, result);
+    }
+    @Test
+    public void petShouldHaveDefaultLitterBox(){
+    Cat underTest = new Cat ("Ben");
+    int result = underTest.litterBox();
+    assertEquals(10, result);
+    }
+    @Test
+    public void cleanLitterBoxShouldDecreaseLitterBoxBy10(){
+    Cat underTest = new Cat ("Ben");
+    underTest.cleanLitterBox();
+    int result = underTest.litterBox();
+    assertEquals(0, result);
+}
 }
