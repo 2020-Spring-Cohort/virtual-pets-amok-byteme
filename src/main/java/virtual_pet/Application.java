ddd42bdcdf7d3e146b6768932b42db222252f557
Noah Doughty <noahdoughty@gmail.com>
2020-01-24 13:08:05 -0500

    Moved cat objects to main app

--- src/main/java/virtual_pet/Application.java
+++ src/main/java/virtual_pet/Application.java
@@ -7,10 +7,27 @@ public class Application {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("test");
+
+        OrganicCat orgCat1 = new OrganicCat("Ben");
+        RoboCat roboCat1 = new RoboCat("Stuart");
+        OrganicCat orgCat2 = new OrganicCat("Noah");
+        RoboCat roboCat2 = new RoboCat("Ernest");
+        OrganicCat orgCat3 = new OrganicCat("Brian");
+        CatShelter shelter = new CatShelter();
+        shelter.addCat(orgCat1);
+        shelter.addCat(roboCat1);
+        shelter.addCat(orgCat2);
+        shelter.addCat(roboCat2);
+        shelter.addCat(orgCat3);
+
+
+
         CatShelter shelterList = new CatShelter();
         for (Cat kitty: shelterList.retrievedCatList()){
             System.out.println(kitty);
         }
+
+
 
 
 
--- src/main/java/virtual_pet/CatShelter.java
+++ src/main/java/virtual_pet/CatShelter.java
@@ -4,31 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CatShelter {
-    private static HashMap<String , Cat> catList = new HashMap<>();
+
     private static ArrayList<Cat> catNames = new ArrayList<>();
 
     public CatShelter () {
 
-        OrganicCat orgCat1 = new OrganicCat("Ben");
-        RoboCat roboCat1 = new RoboCat("Stuart");
-        OrganicCat orgCat2 = new OrganicCat("Noah");
-        RoboCat roboCat2 = new RoboCat("Ernest");
-        OrganicCat orgCat3 = new OrganicCat("Brian");
-
-        catNames.add(orgCat1);
-        catNames.add(roboCat1);
-        catNames.add(orgCat2);
-        catNames.add(roboCat2);
-        catNames.add(orgCat3);
-
     }
 
-    public void addCat(Cat testCat) {catList.put(testCat.getName() , testCat);}
+    public void addCat(Cat testCat) {catNames.add(testCat);}
 
     public static ArrayList<Cat> retrievedCatList() { return catNames; }
 
     public void feedAllCats() {
-        for (Cat catToBeFed : catList.values()) {
+        for (Cat catToBeFed : catNames.get(index)) {
             catToBeFed.receiveFood();
         }
     }
